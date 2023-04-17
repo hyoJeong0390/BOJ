4381faebb6bffb971f705d2363894ea0141de259
mir3u_ <65229910+hyoJeong0390@users.noreply.github.com>
2023-04-17 11:55:41 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,12 @@
+word = input()
+li = []
+for i in range(26):
+    li.append(-1)
+
+for i in range(len(word)):
+    c = ord(word[i])
+    if li[c-97] == -1:
+        li[c-97] = i
+
+for j in range(len(li)):
+    print(li[j], end=' ')
