be2f69c64afc456bce6b8124f5678612b6f3f63a
mir3u_ <65229910+hyoJeong0390@users.noreply.github.com>
2023-04-22 13:35:50 +0900

    Update 공 바꾸기.py

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,10 +1,10 @@
 n, m = map(int, input().split())
 basket = []
 for i in range(n):
-    basket.append(int(i+1))
+    basket.append(int(i+1))  // basket = [*range(1,N+1)]
 for i in range(m):
     i, j = map(int, input().split())
     tmp = basket[i-1]
     basket[i-1] = basket[j-1]
-    basket[j-1] = tmp
+    basket[j-1] = tmp  // basket[i-1],basket[j-1] = basket[j-1],basket[i-1]
 print(*basket)
